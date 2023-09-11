54f83ac9cf2395f140e076709a9205c7d3e51cff
yrisss <114473485+yrisss@users.noreply.github.com>
2023-09-12 00:42:56 +0300

    Update main.c

--- ex6/main.c
+++ ex6/main.c
@@ -2,28 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Структура для описания ВУЗа
 struct University {
     int yearFounded;
     char name[50];
     char type[20];
 };
 
+// Функция для сравнения ВУЗов при сортировке
 int compareUniversities(const void *a, const void *b) {
     const struct University *uniA = (const struct University *)a;
     const struct University *uniB = (const struct University *)b;
 
+    // Сортируем по убыванию года создания
     if (uniA->yearFounded > uniB->yearFounded) return -1;
     if (uniA->yearFounded < uniB->yearFounded) return 1;
 
+    // Если годы создания одинаковые, сортируем по типу
     return strcmp(uniA->type, uniB->type);
 }
 
+// Функция для поиска самого старого института с заданной буквой в конце названия
 void findOldestInstituteWithLetter(struct University *universities, int size, char letter) {
     struct University *oldestInstitute = NULL;
 
     for (int i = 0; i < size; i++) {
-        if (strcmp(universities[i].type, "institute") == 0) {
+        if (strcmp(universities[i].type, "институт") == 0) {
             if (oldestInstitute == NULL || universities[i].yearFounded < oldestInstitute->yearFounded) {
+                // Проверяем, заканчивается ли название на заданную букву
                 int len = strlen(universities[i].name);
                 if (len > 0 && universities[i].name[len - 1] == letter) {
                     oldestInstitute = &universities[i];
@@ -33,50 +39,54 @@ void findOldestInstituteWithLetter(struct University *universities, int size, ch
     }
 
     if (oldestInstitute != NULL) {
-        printf("The oldest institute with a name ending in '%c':\n", letter);
-        printf("Year of creation: %d\n", oldestInstitute->yearFounded);
-        printf("Name: %s\n", oldestInstitute->name);
+        printf("Самый старый институт с названием, заканчивающимся на '%c':\n", letter);
+        printf("Год создания: %d\n", oldestInstitute->yearFounded);
+        printf("Название: %s\n", oldestInstitute->name);
     } else {
-        printf("Institute with select condition not found!\n");
+        printf("Институт с заданным условием не найден.\n");
     }
 }
 
 int main() {
     int size;
-    printf("Enter number of educational establishments: ");
+    printf("Введите количество ВУЗов: ");
     scanf("%d", &size);
 
+    // Выделяем память под динамический массив структурных переменных
     struct University *universities = (struct University *)malloc(size * sizeof(struct University));
 
     if (universities == NULL) {
-        printf("Memory allocation error.\n");
+        printf("Ошибка выделения памяти.\n");
         return 1;
     }
 
+    // Заполняем массив с клавиатуры
     for (int i = 0; i < size; i++) {
-        printf("Enter information about educational establishment %d:\n", i + 1);
-        printf("Year of creation: ");
+        printf("Введите информацию о ВУЗе #%d:\n", i + 1);
+        printf("Год создания: ");
         scanf("%d", &universities[i].yearFounded);
-        printf("Name: ");
+        printf("Название: ");
         scanf("%s", universities[i].name);
-        printf("Type (university, academy, institute): ");
+        printf("Тип (университет, академия, институт): ");
         scanf("%s", universities[i].type);
     }
 
+    // Сортируем ВУЗы по убыванию года создания
     qsort(universities, size, sizeof(struct University), compareUniversities);
 
-    printf("\nEducational establishments after sorting by year of creation and type:\n");
+    printf("\nВУЗы после сортировки по году создания и типу:\n");
     for (int i = 0; i < size; i++) {
-        printf("Year of creation: %d, Name: %s, Type: %s\n",
+        printf("Год создания: %d, Название: %s, Тип: %s\n",
                universities[i].yearFounded, universities[i].name, universities[i].type);
     }
 
     char letter;
-    printf("\nEnter a letter to search for the oldest institute: ");
+    printf("\nВведите букву для поиска самого старого института: ");
     scanf(" %c", &letter);
 
     findOldestInstituteWithLetter(universities, size, letter);
 
+    // Освобождаем выделенную память
     free(universities);
 
     return 0;
